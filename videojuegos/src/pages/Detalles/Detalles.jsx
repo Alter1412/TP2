a9447fce276238676ca2ce2564e435da062c1768
Alter1412 <florianalaforgia@hotmail.com.ar>
2024-05-02 00:39:37 -0300

    boton de home en Detalles

--- videojuegos/src/pages/Detalles/Detalles.jsx
+++ videojuegos/src/pages/Detalles/Detalles.jsx
@@ -1,5 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../../const/routes";
+import  Button from "../../components/Boton/Boton";
+
 const Detalles = () => {
-    return <p className="text-red-700 font-bold text-3xl">detalles...</p>;
+    const navigate = useNavigate();
+
+
+
+const onClickHomeHandler = () => {
+    navigate(ROUTES.home);
+  };
+
+    return( 
+        <><p className="text-red-700 font-bold text-3xl">detalles...</p><Button text="Home" onClick={onClickHomeHandler} /></>
+        );
   };
   
   export default Detalles;
