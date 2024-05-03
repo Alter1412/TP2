5f5bbb98605b13e600afb16f0862d5d648980003
Alter1412 <florianalaforgia@hotmail.com.ar>
2024-05-03 02:36:23 -0300

    acomodacion del tailwind
    faltaban cosas

--- videojuegos/src/components/Card/Card.jsx
+++ videojuegos/src/components/Card/Card.jsx
@@ -6,17 +6,17 @@ const Card = ({juegos}) => {
           
              
             <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <img class="w-full" src={juego.images} alt="" />
-            <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">{juego.title}</div>
-            <p class="text-gray-700 text-base">
-            {juego.description}
-            </p>
-            </div>
-            <div class="px-6 pt-4 pb-2">
+                <img class="w-full" src={juego.images} alt="" />
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">{juego.name}</div>
+                        <p class="text-gray-700 text-base">
+                            {juego.description}
+                        </p>
+                </div>
+                <div class="px-6 pt-4 pb-2">
             
     
-            </div>
+                </div>
             </div>
           
           
