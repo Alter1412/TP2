98f7c1b4ac412e94eea6541688016b363abe3ad3
Alter1412 <florianalaforgia@hotmail.com.ar>
2024-05-02 22:48:50 -0300

    Lista de juegos visible

--- videojuegos/src/pages/Home/Home.jsx
+++ videojuegos/src/pages/Home/Home.jsx
@@ -2,19 +2,43 @@
 import { useNavigate } from "react-router-dom";
 import { ROUTES } from "../../const/routes";
 import  Button from "../../components/Boton/Boton";
+import { useState, useEffect } from "react";
 
 const Home = () => {
   const navigate = useNavigate();
+  const [juegosState, setJuegosState] = useState("");
 
-
+  const fetchJuegos = async () => {
+    console.log("Hola!");
+    const response = await fetch("/mocks/juegos.json");
+    const result = await response.json();
+    setJuegosState(result);
+  };
 
 const onClickDetallesHandler = () => {
     navigate(ROUTES.detalles);
   };
 
-  return ( 
+  useEffect(() => {
+
+    fetchJuegos();
+    
+  }, []);
+
+
+  return (
+  <div>
+
+    {juegosState.map((juegos) => {
+      return <p key={juegos.name}> {juegos.name}</p>;
+    })}
+
     <Button text="detalles" onClick={onClickDetallesHandler} />
-);
+
+    </div>
+
+  )
+
 
 }
 export default Home;
