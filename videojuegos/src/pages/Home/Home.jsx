e04f80af5b2111f99da8dc5cbf2538f325c2709b
Agustina Kilapi Viecens <70908771+agustinakilapi52@users.noreply.github.com>
2024-05-05 21:09:54 -0300

    detalles

--- videojuegos/src/pages/Home/Home.jsx
+++ videojuegos/src/pages/Home/Home.jsx
@@ -1,14 +1,11 @@
-
 import { useNavigate } from "react-router-dom";
-import { ROUTES } from "../../const/routes";
-import  Button from "../../components/Boton/Boton";
+
 import { useState, useEffect } from "react";
 import Card from "../../components/Card/Card";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
+
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 const Home = () => {
-  const navigate = useNavigate();
   const [juegosState, setJuegosState] = useState([]);
   const [filtro, setFiltro] = useState('');//recibe lo que se busca por la barra de busqueda
 
@@ -19,10 +16,23 @@ const Home = () => {
     setJuegosState(result);
   };
 
-const onClickDetallesHandler = () => {
-    navigate(ROUTES.detalles);
+  const busqueda = (event) => {
+    setFiltro(event.target.value);
+    console.log(filtro);
   };
 
+   //filtro de tareas
+   let resultado = []
+   if ( !filtro ) {
+     //utiliza la constante textoFiltro que tiene un useState
+     console.log(juegosState)
+     resultado = juegosState
+   }else{
+     resultado = juegosState.filter((juego)=>juego.name.toLocaleLowerCase().includes(filtro.toLocaleLowerCase()))
+     console.log(resultado)
+   }
+
+  
   useEffect(() => {
 
     fetchJuegos();
@@ -32,21 +42,18 @@ const onClickDetallesHandler = () => {
 
   return (
     
-  <div className="flex flex-wrap">
+  <div class="flex flex-wrap">
     
-    <Header></Header>
+
+    <SearchBar filtro={filtro} onFiltroChange={busqueda}></SearchBar>
 
 {/*     {juegosState.map((juegos) => {
       return <p key={juegos.name}> {juegos.name}</p>;
     })} */}
-    <Card juegos={juegosState}></Card>
+    <Card juegos={resultado}></Card>
 
- 
-
-    </div>
     
-  
-
+   
 
     </div>
 
